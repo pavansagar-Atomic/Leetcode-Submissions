fa6b034215a095f0c206d393d6c1f6f3df263e32
pavansagar-Atomic <pavansagar74834@gmail.com>
2026-09-21 09:14:25 +0530

    Time: 10 ms (100%), Space: 91.9 MB (88.46%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long[] resultArray(int[] nums, int k) {
+        long[] ans = new long[k];
+        long[] dp = new long[k];
+
+        for (int num : nums) {
+            long[] newDp = new long[k];
+            int mod = num % k;
+
+            newDp[mod]++;
+
+            for (int r = 0; r < k; r++) {
+                int newMod = (r * mod) % k;
+                newDp[newMod] += dp[r];
+            }
+
+            for (int r = 0; r < k; r++) {
+                ans[r] += newDp[r];
+            }
+
+            dp = newDp;
+        }
+
+        return ans;
+    }
+}
